dc72b7d1b685acf2b423fdbf480d9b534b59b726
kunj <kunjbarvalia88@gmail.com>
2024-07-18 23:50:06 +0530

    - minor changes

--- Outread/Modules/Flashcard/View/Flashcard.swift
+++ Outread/Modules/Flashcard/View/Flashcard.swift
@@ -26,61 +26,6 @@ struct FlashcardView: View {
         }
     }
     
-    /*
-     private var contentView: some View {
-     Color.COLOR_27394_F
-     .clipShape(RoundedRectangle(cornerRadius: 20))
-     .padding(.horizontal, 15)
-     .padding(.bottom, 10)
-     .overlay {
-     if !viewModel.list.isEmpty {
-     articalView
-     } else {
-     VStack(spacing: 10) {
-     Text("Test Tes Test Test Test")
-     .foregroundColor(Color.white)
-     .font(.poppins(weight: .medium, size: 27))
-     .multilineTextAlignment(.center)
-     
-     Text("Test Tes Test Test Test Test Tes Test Test Test Test Tes Test Test Test Test Tes Test Test Test Test Tes Test Test Test Test Tes Test Test Test Test Tes Test Test Test Test Tes Test Test Test Test Tes Test Test Test Test Tes Test Test Test")
-     .foregroundColor(Color.white)
-     .font(.poppins(weight: .regular, size: 17))
-     
-     Spacer()
-     }
-     .padding(.bottom, 40)
-     .padding(.top, 10)
-     .padding(.horizontal, 32)
-     }
-     }
-     }
-     */
-    
-    /*
-     private var articalView: some View {
-     TabView {
-     ForEach(viewModel.list, id: \.self) { listData in
-     ScrollView(showsIndicators: false) {
-     VStack(spacing: 10) {
-     Text(listData.str1)
-     .foregroundColor(Color.white)
-     .font(.poppins(weight: .medium, size: 27))
-     .multilineTextAlignment(.center)
-     
-     Text(listData.str2)
-     .foregroundColor(Color.white)
-     .font(.poppins(weight: .regular, size: 17))
-     }
-     }
-     .padding(.bottom, 40)
-     .padding(.top, 10)
-     }
-     }
-     .padding(.horizontal, 32)
-     .tabViewStyle(.page(indexDisplayMode: .automatic))
-     }
-     */
-    
     private var articalView: some View {
         GeometryReader { geo in
             ScrollView(showsIndicators: false) {
